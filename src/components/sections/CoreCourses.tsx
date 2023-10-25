1f4befe696c22e275effe6fca2d637146272b4c4
Aayman-star <aayman.khalid1@gmail.com>
2023-10-25 19:09:11 +0500

    completed the core courses section

--- src/components/sections/CoreCourses.tsx
+++ src/components/sections/CoreCourses.tsx
@@ -12,46 +12,58 @@ import {
 const CoreCourses = () => {
   return (
     <Wrapper>
-      <section className="max-w-7xl mx-auto p-2 grid grid-cols-3 gap-x-2">
-        <Card className="border border-heroblue font-Poppins grid place-content-center">
+      <section className="max-w-7xl mx-auto p-2 ">
+        <Card className="border-none text-center">
           <CardHeader>
-            <CardTitle className="text-heroblue text-center">
-              Quarter-I
+            <CardTitle className="font-semibold text-heroblue">
+              Core Courses
             </CardTitle>
+            <CardDescription className="text-heroblue">
+              Common in all specialisations
+            </CardDescription>
           </CardHeader>
-          <CardContent>
-            <p className="text-heroblue text-center">
-              CS-101: Object-Oriented Programming using TypeScript
-            </p>
-          </CardContent>
         </Card>
-        <Card className="border border-heroblue font-Poppins grid place-content-center">
-          <CardHeader>
-            <CardTitle className="text-heroblue text-center">
-              Quarter-II
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p className="text-heroblue text-center">
-              W2-201: Developing Planet-Scale Web 2.0 Serverless Cloud Cloud
-              Apps and APIs using Next.js 13 and Cloud Development Kit (CDK) for
-              Terraform
-            </p>
-          </CardContent>
-        </Card>
-        <Card className="border border-heroblue font-Poppins grid place-content-center">
-          <CardHeader>
-            <CardTitle className="text-heroblue text-center">
-              Quarter-III
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p className="text-heroblue text-center">
-              $-101: Dollar Making Bootcamp - Full-Stack Template and API
-              Product Developmentl
-            </p>
-          </CardContent>
-        </Card>
+        <div className="grid grid-cols-3 gap-x-2">
+          <Card className="border border-heroblue font-Poppins grid place-content-center">
+            <CardHeader>
+              <CardTitle className="text-heroblue text-center font-medium">
+                Quarter-I
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="text-heroblue text-center">
+                CS-101: Object-Oriented Programming using TypeScript
+              </p>
+            </CardContent>
+          </Card>
+          <Card className="border border-heroblue font-Poppins grid place-content-center">
+            <CardHeader>
+              <CardTitle className="text-heroblue text-center font-medium">
+                Quarter-II
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="text-heroblue text-center">
+                W2-201: Developing Planet-Scale Web 2.0 Serverless Cloud Cloud
+                Apps and APIs using Next.js 13 and Cloud Development Kit (CDK)
+                for Terraform
+              </p>
+            </CardContent>
+          </Card>
+          <Card className="border border-heroblue font-Poppins grid place-content-center">
+            <CardHeader>
+              <CardTitle className="text-heroblue text-center font-medium">
+                Quarter-III
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="text-heroblue text-center">
+                $-101: Dollar Making Bootcamp - Full-Stack Template and API
+                Product Developmentl
+              </p>
+            </CardContent>
+          </Card>
+        </div>
       </section>
     </Wrapper>
   );
